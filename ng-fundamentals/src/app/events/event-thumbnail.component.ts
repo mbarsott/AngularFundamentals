3a10e69c7b051ac2f25829ecf637836866ccd1b9
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-11-04 17:08:51 -0500

    4. Exploring the Angular Template Syntax

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -4,17 +4,29 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
   selector: "event-thumbnail",
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{ event.name }}</h2>
-      <div>Date: {{ event.date }}</div>
-      <div>Time: {{ event.time }}</div>
-      <div>Price: \${{ event.price }}</div>
-      <div>
-        <span>Location: {{ event.location.address }}</span>
+      <h2>{{ event?.name }}</h2>
+      <div>Date: {{ event?.date }}</div>
+      <!--div [class.green]="event?.time === '8:00 am'" [ngSwitch]="event?.time"-->
+      <!--div [ngClass]="getStartTimeClasses()" [ngSwitch]="event?.time"-->
+      <!--div [style.color]="event?.time === '8:00 am' ? '#003300' : '#bbb'" [ngSwitch]="event?.time"-->
+      <!--div [ngStyle]="{'color': event?.time === '8:00 am' ? '#003300' : '#bbb', 'font-weight': event?.time === '8:00 am' ? 'bold' : 'normal'}" [ngSwitch]="event?.time"-->
+      <div [ngStyle]="getStartTimeStyles()" [ngSwitch]="event?.time">
+        Time: {{ event?.time }}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: \${{ event?.price }}</div>
+      <div *ngIf="event?.location">
+        <!--div [hidden]="!event?.location"-->
+        <span>Location: {{ event?.location?.address }}</span>
         <!--span>&nbsp;</span-->
         <span class="pad-left"
-          >{{ event.location.city }}, {{ event.location.country }}</span
+          >{{ event?.location?.city }}, {{ event?.location?.country }}</span
         >
       </div>
+      <div *ngIf="event?.onlineUrl">Online URL: {{ event?.onlineUrl }}</div>
+      <!--div [hidden]="!event?.onlineUrl">Online URL: {{ event?.onlineUrl }}</div-->
       <!--button class="btn btn-primary" (click)="handleClickMe()">
         Clickme!
       </button-->
@@ -22,17 +34,47 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
   `,
   styles: [
     `
+      .green {
+        color: #003300 !important;
+      }
+      .bold {
+        font-weight: bold;
+      }
+      .thumbnail {
+        min-height: 210px;
+      }
       .pad-left {
         margin-left: 10px;
       }
       .well div {
-         color: #bbb;
+        color: #bbb;
       }
     `
   ]
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeStyles(): any {
+    if (this.event && this.event.time === "8:00 am") {
+      return { color: "#003300", "font-weight": "bold" };
+    } else {
+      return {};
+    }
+  }
+
+  getStartTimeClasses() {
+    // const isEarlyStart = this.event && this.event.time === '8:00 am';
+    // return {green: isEarlyStart, bold: isEarlyStart};
+    // if (this.event && this.event.time === '8:00 am') {
+    //   return 'green bold';
+    // } else {return ''; }
+    if (this.event && this.event.time === "8:00 am") {
+      return ["green", "bold"];
+    } else {
+      return [];
+    }
+  }
   // @Output() eventClick = new EventEmitter();
   // someProperty: any = "Some value";
 
