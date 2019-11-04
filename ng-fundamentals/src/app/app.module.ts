af082b0dc154a79999050470cc4979b10f6cfdda
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-11-04 15:46:04 -0500

    Fix issues that prevent successful build

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from "@angular/core";
 import { EventsAppComponent } from "./events-app.component";
 import { EventsListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/event-thumbnail.component";
-import { NavBarComponent } from "./nav/navbar.component";
+import { NavBarComponent } from "./nav/nav-bar.component";
 
 @NgModule({
   imports: [BrowserModule],
