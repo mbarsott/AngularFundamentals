a09ae90f71a68c5b28da6fe02c23bde5093434f4
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2020-02-12 12:44:25 -0500

    14.3. Moving Data Storage to the Se...

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import {
   EventsListComponent,
@@ -40,7 +41,8 @@ const jQuery = window["$"];
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   declarations: [
     EventsAppComponent,
